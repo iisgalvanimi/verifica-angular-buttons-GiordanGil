156ae6b16ae3ef62fb17b2e4db0f6661d7615964
GiordanGil <gilerlozano.giordan@iisgalvanimi.edu.it>
2022-03-17 09:05:00 +0000

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Empty-Project';
+  nombtn = ""
+  lista: string [] = []
+
+  btn1(){
+    this.nombtn = "Button 1"
+    this.lista.push(this.nombtn)
+  }
+  btn2(){
+    this.nombtn = "Button 2"
+    this.lista.push(this.nombtn)
+  }
+  btn3(){
+    this.nombtn = "Button 3"
+    this.lista.push(this.nombtn)
+  }
+  btn4(){
+    this.nombtn = "Button 4"
+    this.lista.push(this.nombtn)
+  }
+
 }
